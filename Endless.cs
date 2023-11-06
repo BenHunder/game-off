0bbceb8cac5c1e3234e168e9ed09863bcc674037
Ben Hunder <benhunder@gmail.com>
2023-11-05 23:07:23 -0600

    displaying held keys

--- Endless.cs
+++ Endless.cs
@@ -4,10 +4,13 @@ using System;
 public partial class Endless : Node2D
 {
 	PackedScene hold;
+	string leftHand;
+	string rightHand;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		hold = (PackedScene)GD.Load("res://Hold.tscn");
+		//hold = (PackedScene)GD.Load("res://Hold.tscn");
+		leftHand = "";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,12 +21,32 @@ public partial class Endless : Node2D
 	
 	public override void _UnhandledInput(InputEvent @event)
 	{
+		//GD.Print("event: " + @event);
 		if(@event is InputEventMouseButton mouseEvent)
 		{
-			GD.Print(hold);
-			Node2D holdNode = (Node2D)hold.Instantiate();
-			holdNode.Position = mouseEvent.Position;
-			this.AddChild(holdNode);
+			
+			//Node2D holdNode = (Node2D)hold.Instantiate();
+			//holdNode.Position = mouseEvent.Position;
+			//this.AddChild(holdNode);
 		}
+		
+		if(@event is InputEventKey keyEvent)
+		{
+			if (keyEvent.Pressed && !keyEvent.Echo)
+			{
+				string c = keyEvent.Keycode.ToString();
+				if(!leftHand.Contains(c)) leftHand += c;
+			}
+			
+			if (!keyEvent.Pressed && !keyEvent.Echo)
+			{
+				string c = keyEvent.Keycode.ToString();
+				if(leftHand.Contains(c)) leftHand = leftHand.Replace(c,"");
+			}
+			
+			RichTextLabel t = GetNode<RichTextLabel>("RichTextLabel");
+			t.Text = leftHand;
+		}
+		
 	}
 }
