a89fa81c2c19cdb6f4e6729fe181a2a6acb37d6f
Ben Hunder <benhunder@gmail.com>
2023-11-07 22:20:05 -0600

    scrolling holds

--- Endless.cs
+++ Endless.cs
@@ -6,103 +6,90 @@ using System.Collections.Generic;
 
 public partial class Endless : Node2D
 {
-	PackedScene hold;
-	Hand leftHand;
-	Hand rightHand;
-	
-	List<Key> leftCharacters = new List<Key>()
-	{
-		Key.Q,
-		Key.W,
-		Key.E,
-		Key.R,
-		Key.T,
-		Key.A,
-		Key.S,
-		Key.D,
-		Key.F,
-		Key.G,
-		Key.Z,
-		Key.X,
-		Key.C,
-		Key.V,
-		Key.B	
-	};
-	List<Key> rightCharacters = new List<Key>()
-	{
-		Key.Y,
-		Key.U,
-		Key.I,
-		Key.O,
-		Key.P,
-		Key.H,
-		Key.J,
-		Key.K,
-		Key.L,
-		Key.Semicolon,
-		Key.N,
-		Key.M,
-		Key.Comma,
-		Key.Period,
-		Key.Slash	
-	};
+	Hand _leftHand;
+	Hand _rightHand;
+	Hand _feet;
+
+	WallManager _wallManager;
+	PackedScene _popUp;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		hold = (PackedScene)GD.Load("res://Hold.tscn");
-		leftHand = new Hand();
-		rightHand = new Hand();
+		_wallManager = GetNode<WallManager>("WallManager");
+		_popUp = (PackedScene)GD.Load("res://TextPopup.tscn");
+		_leftHand = new Hand();
+		_rightHand = new Hand();
+		_feet = new Hand();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
+		if(HasLost())
+		{
+			PopupPanel p = (PopupPanel)_popUp.Instantiate();
+			CallDeferred("add_child", p);
+			}
 	}
 	
 	public override void _UnhandledInput(InputEvent @event)
 	{
-		//GD.Print("event: " + @event);
 		if(@event is InputEventMouseButton mouseEvent)
 		{
+			_wallManager.CreateHold(mouseEvent.Position);
 			
-			Node2D holdNode = (Node2D)hold.Instantiate();
-			holdNode.Position = mouseEvent.Position;
-			this.AddChild(holdNode);
 		}
 		
 		if(@event is InputEventKey keyEvent)
 		{
+			GD.Print("key press");
 			if (keyEvent.Pressed && !keyEvent.Echo) HandleKeyPressed(keyEvent.Keycode);
 			
 			if (!keyEvent.Pressed && !keyEvent.Echo) HandleKeyReleased(keyEvent.Keycode);
-			
-			RichTextLabel t = GetNode<RichTextLabel>("RichTextLabel");
-			t.Text = "left: " + leftHand + " right: " + rightHand;
+		
+			RichTextLabel t = GetNode<RichTextLabel>("DebugInfo");
+			t.Text = "left: " + _leftHand + " right: " + _rightHand + " feet: " + _feet;
 		}
 	}
+
+	private bool HasLost() => _wallManager.GameStarted && _leftHand.IsEmpty && _rightHand.IsEmpty && _feet.IsEmpty;
 	
 	private void HandleKeyPressed(Key k)
 	{
-		if(leftCharacters.Contains(k) && !leftHand.Fingers.Contains(k)) leftHand.Fingers.Add(k);
-		else if(rightCharacters.Contains(k) && !rightHand.Fingers.Contains(k)) rightHand.Fingers.Add(k);
+		Regions limb = _wallManager.GetLimbForKey(k);
+		GD.Print("pressed " + k + " for " + limb);
+		if(limb == Regions.Left && !_leftHand.Contains(k)) _leftHand.Add(k);
+		else if(limb == Regions.Right && !_rightHand.Contains(k)) _rightHand.Add(k);
+		else if(limb == Regions.Middle && !_feet.Contains(k)) _feet.Add(k);
+		_wallManager.GameStarted = true;
 	}
 	private void HandleKeyReleased(Key k)
 	{
-		if(leftCharacters.Contains(k) && leftHand.Fingers.Contains(k)) leftHand.Fingers.Remove(k);
-		else if(rightCharacters.Contains(k) && rightHand.Fingers.Contains(k)) rightHand.Fingers.Remove(k);
+		Regions limb = _wallManager.GetLimbForKey(k);
+		if(limb == Regions.Left && _leftHand.Contains(k)) _leftHand.Remove(k);
+		else if(limb == Regions.Right && _rightHand.Contains(k)) _rightHand.Remove(k);
+		else if(limb == Regions.Middle && _feet.Contains(k)) _feet.Remove(k);
 	}
 
 	public class Hand
 	{
-		public List<Key> Fingers;
+		public bool IsEmpty {get { return !_fingers.Any(); }}
+		private List<Key> _fingers;
+		private int _nKeys = 1;
 
 		public Hand()
 		{
-			Fingers = new List<Key>();
+			_fingers = new List<Key>();
 		}
-
-		public override string ToString() => Fingers.Aggregate("", (acc, k) => acc + k.ToString());
+		public void Add(Key k)
+		{
+			GD.Print("adding; " + k);
+			if(_fingers.Count() >= _nKeys) _fingers.RemoveAt(0);
+			_fingers.Add(k);
+		}
+		public void Remove(Key k) => _fingers.Remove(k);
+		public bool Contains(Key k) => _fingers.Contains(k);
+		public override string ToString() => _fingers.Aggregate("", (acc, k) => acc + k.ToString());
 	}
 }
---
+++ Enums.cs
@@ -0,0 +1,7 @@
+public enum Regions
+{
+    None=0,
+    Left=1,
+    Middle=2,
+    Right=3
+}
